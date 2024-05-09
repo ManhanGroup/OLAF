800086753fb88827ffba98fa3c60dd8bdd3dee7e
Colby Brown <colby@manhangroup.com>
2024-05-09 07:08:53 -0400

    simplify and complete

--- main.py
+++ main.py
@@ -9,30 +9,28 @@ class OLAF_model:
         # Examples: number of zones, number of product types
         # Probabaly can use this to load initial supply inventory and capacity values by product type
         self.config = load_yaml(config)
-        self.base_df = gpd.read_file(self.config['zonal_inputs'])
-        # Note on control totals: rather than a single "growth" value, include both growth and decline to allow churn
-        self.product_types = self.config['product_types']
+        self.zone_df = gpd.read_file(self.config['zonal_inputs'])
+        self.zone_df.set_index(self.config['geo_id'])
+        self.land_uses = self.config['land_uses']
     def validate(self):
         # This method may contain functions to verify that the internal data structures are consistent
         pass
-    def evaluate(self):
-        # Evaluates the supply inventory in its current state, including scoring the utility of development options
-        # A lot of customization should be allowed here--ideally, the user can write their own custom scoring function
-        # Ultimately, an entire bid-rent analysis might just plug-in here
-        pass
-    def enumerate(self,k,v):
-        # This method enumerates k development options for product type v, within build-out capacities (reflecting filters)
+    def enumerate(self,n,land_use):
+        # This method enumerates n development options for a land use, within build-out capacities (reflecting filters)
         # Valid development options are a subset of all zones and product types
-        pass
-    def denumerate(self,k,v):
-        # This method enumerates k potential redevelopment sites for inventory product type v, respecting appropriate filters
-        pass
-    def undevelop(self):
-        # This method allocates negative control total "stocks" to development option "sinks" using Monte Carlo simulation
-        # method is the same as below, except:
-        # - only options that are non-zero are eligible for removal from inventory
-        # - sign of score is flipped so that lower-utility options are likelier for selection
-        pass
+        capacity_field = self.land_uses["land_use"]["capacity"]
+        inventory_field = self.land_uses["land_use"]["inventory"]
+        potential = np.maximum(self.zone_df[capacity_field] - self.zone_df[inventory_field], 0)
+        options = zone_df.sample(n,weights=potential)
+        return(options)
     def allocate(self):
-        # This method allocates positive control total "stocks" to development option "sinks" using Monte Carlo simulation
-        pass
+        # This method allocates land use control totals using Monte Carlo simulation
+        for land_use in self.land_uses:
+            value_fn = self.land_uses[land_use]["value_fn"]
+            growth = int(self.land_uses[land_use]["growth"])
+            for draw in range(growth):
+                options = self.enumerate(30,land_use)
+                utility = options.eval(value_fn)
+                expUtil = np.exp(utility)
+                zoneSel = self.zone_df.sample(1,weights=expUtil)[self.config['geo_id']]
+                self.zone_df[self.config['geo_id']==zoneSel,:][self.land_uses[land_use]["inventory"]] += 1
